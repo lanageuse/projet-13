6aa221a513039d1b1bb14c10a0579dbb32e8ebef
Dev Lanageuse <dev@lanageuse.com>
2025-07-02 13:25:08 +0200

    feat: integrate useUser hook in Title component to display dynamic user name

--- src/components/UI/Title.tsx
+++ src/components/UI/Title.tsx
@@ -1,10 +1,13 @@
 import React, { type JSX } from 'react';
+import useUser from '../../hooks/useUser';
 
 const Title: React.FC = (): JSX.Element => {
+  const { state } = useUser(18);
+  const { data } = state;
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-5xl font-bold text-black">
-        Bonjour <span className="text-red">Thomas</span>
+        Bonjour <span className="text-red">{data?.userInfos.firstName}</span>
       </h1>
       <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
     </div>
