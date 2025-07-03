d37b9cd0e804fb228ce0a52244c98f55435dc290
Dev Lanageuse <dev@lanageuse.com>
2025-07-03 22:01:54 +0200

    feat: add ScoreData interface for ScoreChart component

---
+++ src/types/charts/scoreData.ts
@@ -0,0 +1,8 @@
+/**
+ * Cette interface est utilisée pour typer les données passées aux composants de ScoreChart
+ * @interface ScoreData
+ */
+export interface ScoreData {
+  score: number | undefined;
+  fill: string;
+}
