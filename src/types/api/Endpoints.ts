98fa4e5ec3a64a3c123753a1ce9c19acd31ebeb1
Dev Lanageuse <dev@lanageuse.com>
2025-07-03 22:00:59 +0200

    fix: remove extra dot in User endpoint URL

--- src/types/api/Endpoints.ts
+++ src/types/api/Endpoints.ts
@@ -1,6 +1,6 @@
 const domain = import.meta.env.VITE_BASE_URL;
 export const ApiEndpoints = {
-  User: `${domain}./user/:userId`,
+  User: `${domain}/user/:userId`,
   UserActivity: `${domain}/user/:userId/activity`,
   UserAverageSessions: `${domain}/user/:userId/average-sessions`,
   UserPerformance: `${domain}/user/:userId/performance`,
