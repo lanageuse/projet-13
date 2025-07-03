253b45eceffc659af98719ccafe026e472363f82
Dev Lanageuse <dev@lanageuse.com>
2025-07-03 11:13:34 +0200

    feat: add UserContext for managing user state across components

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from 'react';
+import useFetch from '../hooks/useFetch';
+import { ApiEndpoints } from '../types/api/Endpoints';
+import { type UserData, authorizedId } from '../types/api/User';
+
+const UserContext = createContext<UserData | null>(null);
+
+export const UserProvider = ({ children }: React.PropsWithChildren) => {
+  const { state } = useFetch<UserData>(authorizedId.cecilia, ApiEndpoints.User);
+  const { data } = state;
+  return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('le composant doit être dans le userProvider');
+  }
+  return context;
+};
