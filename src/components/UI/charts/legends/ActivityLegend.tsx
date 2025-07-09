69285a4cb0febaafcd4b754a08a044050ca6eab6
Dev Lanageuse <dev@lanageuse.com>
2025-07-09 10:03:51 +0200

    refact : add title for group title and legend in the same component

--- src/components/UI/charts/legends/ActivityLegend.tsx
+++ src/components/UI/charts/legends/ActivityLegend.tsx
@@ -14,16 +14,21 @@ export const CustomActivityLegend = (props: ActivityLegend) => {
   // Pas d'affichage si ale payload n'est pas fourni
   if (!payload || payload.length === 0) return null;
   return (
-    <div className="flex flex-row-reverse justify-start gap-3">
-      {payload?.map((item: LegendPayloadItem, index: number) => (
-        <div key={index} className="flex items-center gap-1 text-sm">
-          <span
-            style={{ backgroundColor: item.color }}
-            className="inline-block h-2 w-2 rounded-2xl"
-          />
-          {item.value}
-        </div>
-      ))}
+    <div className="flex flex-col-reverse flex-wrap justify-between gap-3 md:flex-row-reverse">
+      <div className="flex w-full items-center justify-center gap-3 md:w-fit">
+        {payload?.map((item: LegendPayloadItem, index: number) => (
+          <div key={index} className="gap-1text-sm flex items-center gap-1">
+            <span
+              style={{ backgroundColor: item.color }}
+              className="inline-block h-2 w-2 rounded-2xl"
+            />
+            {item.value}
+          </div>
+        ))}
+      </div>
+      <div className="w-full text-center text-[15px] font-medium md:w-fit md:text-left">
+        Activité quotidienne
+      </div>
     </div>
   );
 };
