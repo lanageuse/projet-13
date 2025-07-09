01a15275f5f32075476bca6f9a2e7d365ca3a3e3
Dev Lanageuse <dev@lanageuse.com>
2025-07-09 09:08:34 +0200

    refact : adjust margin and height

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -18,18 +18,18 @@ function Main() {
       <Header />
       <div className="flex flex-row">
         <SideBar />
-        <div className="ml-32 flex w-full flex-col gap-8 lg:px-24 lg:py-16">
+        <div className="flex w-full flex-col gap-8 px-6 py-6 sm:ml-32 lg:px-24 lg:py-16">
           <Title />
-          <div className="flex gap-8">
+          <div className="flex flex-col gap-8 xl:flex-row">
             <div className="flex flex-3/4 flex-col gap-8">
-              <div className="rounded-chart h-80 bg-gray-700 p-6">
+              <div className="rounded-chart bg-gray-700 p-6">
                 <ActivityChart />
               </div>
-              <div className="flex h-60 flex-col gap-8 lg:flex-row">
+              <div className="flex flex-col gap-8 lg:flex-row">
                 <div className="bg-red rounded-chart flex-1/3">
                   <SessionChart />
                 </div>
-                <div className="bg-blue rounded-chart flex-1/3 pt-2">
+                <div className="bg-blue rounded-chart flex-1/3 p-2">
                   <PerformanceChart />
                 </div>
                 <div className="rounded-chart relative flex-1/3 bg-gray-700">
