9c8d714d55f5d0fdc07df7664702fced43b3e16d
Dev Lanageuse <dev@lanageuse.com>
2025-08-04 17:24:23 +0200

    refact: corrige la documentation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,14 @@
 /**
- * Remplace le placeholder :userId dans l'URL par l'identifiant utilisateur de l'environnement.
+ * Remplace  :userId dans l'URL par l'id utilisateur de l'environnement.
  * @param {string} path Chemin d'endpoint contenant :userId
  * @returns {string} Chemin avec l'identifiant utilisateur injecté
  */
 export const buildEndpoint = (path: string) => {
   const user_id = import.meta.env.VITE_USER_ID;
   return path.replace(':userId', user_id);
+};
+
+export const buildMockEndpoint = (path: string) => {
+  const mockPath = buildEndpoint(path);
+  return mockPath.replace(import.meta.env.VITE_BASE_URL, '');
 };
