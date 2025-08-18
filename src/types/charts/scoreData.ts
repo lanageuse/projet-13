16b0bedbecbaf399bcc1f63799e28ff62613a155
Dev Lanageuse <dev@lanageuse.com>
2025-08-18 14:54:02 +0200

    fix: mise à jour de l'interface ScoreData pour corriger les propriétés score et fill

--- src/types/charts/scoreData.ts
+++ src/types/charts/scoreData.ts
@@ -2,6 +2,6 @@
  * Cette interface est utilisée pour typer les données passées aux composants de ScoreChart
  */
 export interface ScoreData {
-  score: number | undefined;
-  fill: string;
+  percent: number | undefined;
+  fill: string | undefined;
 }
