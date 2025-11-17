5faed2b826b186e467b397677b4f787052bcea7f
Dev Lanageuse <dev@lanageuse.com>
2025-11-17 17:25:18 +0100

    feat: ajout du lazy loading pour optimiser le bundle

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
-import ActivityChart from '../components/charts/ActivityChart';
-import PerformanceChart from '../components/charts/PerformanceChart';
-import ScoreChart from '../components/charts/ScoreChart';
-import SessionChart from '../components/charts/SessionChart';
-import Header from './Header';
-import SideBar from './SideBar';
-import Title from './Title';
-import KpiList from '../components/kpi/KpiList';
+import { lazy, Suspense } from 'react';
+
+const ActivityChart = lazy(() => import('../components/charts/ActivityChart'));
+const PerformanceChart = lazy(
+  () => import('../components/charts/PerformanceChart')
+);
+const ScoreChart = lazy(() => import('../components/charts/ScoreChart'));
+const SessionChart = lazy(() => import('../components/charts/SessionChart'));
+const Header = lazy(() => import('./Header'));
+const SideBar = lazy(() => import('./SideBar'));
+const Title = lazy(() => import('./Title'));
+const KpiList = lazy(() => import('../components/kpi/KpiList'));
 import { DashboardStatus } from '../components/DashboardStatus';
 
 /**
@@ -20,30 +24,40 @@ function MainLayout() {
       <div className="flex flex-row">
         <SideBar />
         <DashboardStatus>
-        <div className="flex w-full flex-col gap-8 px-6 py-6 sm:ml-32 lg:px-24 lg:py-16">
-          <Title />
-          <div className="flex flex-col gap-8 xl:flex-row">
-            <div className="flex flex-3/4 flex-col gap-8">
-              <div className="rounded-chart bg-gray-700 p-6">
-                <ActivityChart />
+          <div className="flex w-full flex-col gap-8 px-6 py-6 sm:ml-32 lg:px-24 lg:py-16">
+            <Title />
+            <div className="flex flex-col gap-8 xl:flex-row">
+              <div className="flex flex-3/4 flex-col gap-8">
+                <div className="rounded-chart bg-gray-700 p-6">
+                  <Suspense fallback="Chargement...">
+                    <ActivityChart />
+                  </Suspense>
+                </div>
+                <div className="flex flex-col gap-8 lg:flex-row">
+                  <div className="bg-red rounded-chart flex-1/3">
+                    <Suspense fallback="Chargement...">
+                      <SessionChart />
+                    </Suspense>
+                  </div>
+                  <div className="bg-blue rounded-chart flex-1/3 p-2">
+                    <Suspense fallback="Chargement...">
+                      <PerformanceChart />
+                    </Suspense>
+                  </div>
+                  <div className="rounded-chart relative flex-1/3 bg-gray-700">
+                    <Suspense fallback="Chargement...">
+                      <ScoreChart />
+                    </Suspense>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col gap-8 lg:flex-row">
-                <div className="bg-red rounded-chart flex-1/3">
-                  <SessionChart />
-                </div>
-                <div className="bg-blue rounded-chart flex-1/3 p-2">
-                  <PerformanceChart />
-                </div>
-                <div className="rounded-chart relative flex-1/3 bg-gray-700">
-                  <ScoreChart />
-                </div>
+              <div className="flex flex-1/4 flex-col gap-10">
+                <Suspense fallback="Chargement...">
+                  <KpiList />
+                </Suspense>
               </div>
-            </div>
-            <div className="flex flex-1/4 flex-col gap-10">
-              <KpiList />
             </div>
           </div>
-        </div>
         </DashboardStatus>
       </div>
     </div>
