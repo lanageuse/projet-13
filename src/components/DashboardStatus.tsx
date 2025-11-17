b21247afbbb7488d745fe1b12bf8904b88db14ac
Dev Lanageuse <dev@lanageuse.com>
2025-11-17 19:07:11 +0100

    refactor(DashboardStatus) : modification pour qu'il prenne la totalité de l'écran

--- src/components/DashboardStatus.tsx
+++ src/components/DashboardStatus.tsx
@@ -10,7 +10,7 @@ export const DashboardStatus = ({ children }: { children: React.ReactNode }) => 
   // État de chargement
   if (isLoading) {
     return (
-      <div className="flex min-h-screen items-center justify-center">
+      <div className="flex min-h-screen items-center justify-center ">
         <div className="text-center">
           <div className="mb-4 inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-red-600 border-r-transparent"></div>
           <p className="text-lg text-gray-600">Chargement des données...</p>
@@ -22,7 +22,7 @@ export const DashboardStatus = ({ children }: { children: React.ReactNode }) => 
   // État d'erreur
   if (hasError) {
     return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex min-h-screen items-center justify-center bg-gray-100 w-full ml-32">
         <div className="max-w-md rounded-lg bg-white p-8 shadow-lg">
           <h2 className="mb-4 text-2xl font-bold text-red-600">
             Erreur de chargement
